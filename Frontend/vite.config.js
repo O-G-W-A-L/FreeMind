a0d1ab0b05193915614460aa58c5a28cf1772620
O-G-W-A-L <ogwalja04@gmail.com>
2024-09-19 16:53:42 +0300

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Backend server URL
+        target: 'http://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
